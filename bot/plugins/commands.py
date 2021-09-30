511ac1de6256002e9875902fb44e4093ffb520a9
Anderson-Johny-George17 <91043752+Anderson-Johny-George17@users.noreply.github.com>
2021-09-30 20:44:45 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -118,7 +118,7 @@ Join on our channel to get movies ✅
 
                 document = file_id,
 
-                caption = caption,
+                caption = caption /n @flix_updates,
 
                 parse_mode="html",
 
@@ -158,7 +158,7 @@ Join on our channel to get movies ✅
 
                 video = file_id,
 
-                caption = caption,
+                caption = caption /n @flix_updates,
 
                 parse_mode="html",
 
