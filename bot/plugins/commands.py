1eb947d5fc7a50a8248080cf9d1e841fc1157bd6
Anderson-Johny-George17 <91043752+Anderson-Johny-George17@users.noreply.github.com>
2021-10-01 16:32:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -134,7 +134,7 @@ Join on our channel to get movies ✅
 
                                 (
 
-                                    'Developers', url="https://t.me/JNS_BOTS"
+                                    'Developers', url="https://t.me/flix_updates"
 
                                 )
 
@@ -172,7 +172,7 @@ Join on our channel to get movies ✅
 
                                 (
 
-                                    'Developers', url="https://t.me/jns_bots"
+                                    'Developers', url="https://t.me/flix_updates"
 
                                 )
 
@@ -210,7 +210,7 @@ Join on our channel to get movies ✅
 
                                 (
 
-                                    'Developers', url="https://t.me/jns_bots"
+                                    'Developers', url="https://t.me/flix_updates"
 
                                 )
 
@@ -242,7 +242,7 @@ Join on our channel to get movies ✅
 
      [
 
-        InlineKeyboardButton('Support 🛠', url='https://t.me/jns_fc_bots')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/flix_corner')
 
     ],[
 
